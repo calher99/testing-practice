28af5e4b7aa57a97aa4495af0bb95714a103476c
calher99 <calher99@gmail.com>
2022-12-22 16:20:45 +0000

    implement function to sort if string is uppercase or a symbol

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,21 +1,41 @@
 const caesarCipher = (string,shift) =>{
     let result='';
     for(let i = 0; i<string.length; i++){
-        result += cipherLetter(string[i],shift)
+        if(string[i]!==' '){
+            result += cipherLetter(string[i],shift)
+        }else{
+            result +=' '
+        }
     }
     return result;
 }
 
 const cipherLetter = (letter,shift) =>{
+
     const index = letter.charCodeAt(0);
+    let replacement = ''
+
+    if (index>=97 && index <=122){//lowercase
+        replacement = modifyString(index,shift,122,97)
+    }else if (index>=65 && index <=90){//uppercase
+        replacement = modifyString(index,shift,90,65)
+    }else{//something weird
+        replacement = letter;
+    }
+    return replacement;
+}
+
+function modifyString (index,shift,max,min){
     const newIndex= index+shift;
-    if(newIndex>122){
-        const newShift = newIndex-123;
-        const replacement = String.fromCharCode(97+newShift);
+    if(newIndex>max){ //case it goes above Z
+        const newShift = newIndex-(max+1);
+        const replacement = String.fromCharCode(min+newShift);
         return replacement;
     }
+    //normal case
     const replacement = String.fromCharCode(index+shift)
     return replacement;
 }
+
 
 export default caesarCipher
--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -11,3 +11,15 @@ test('cipher several letters', () => {
 test('cipher letters at end of alphabet', () => {
     expect(caesarCipher('yz' , 3)).toBe('bc');
 })
+
+test('cipher sentence', () => {
+    expect(caesarCipher('my name is carlos' , 8)).toBe('ug vium qa kiztwa');
+})
+
+test('cipher uppercase', () => {
+    expect(caesarCipher('LOreM' , 7)).toBe('SVylT');
+})
+
+test('accept symbols and numbers', () => {
+    expect(caesarCipher('LOreM59/' , 7)).toBe('SVylT59/');
+})
